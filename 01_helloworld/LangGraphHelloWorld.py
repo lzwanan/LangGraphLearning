9e53613b8c9106962ede61d7bc4e72a2ed82cd0a
lzwanan <lzwanan@outlook.com>
2026-08-26 21:45:16 +0800

    Hello LangGraph

--- 01_helloworld/LangGraphHelloWorld.py
+++ 01_helloworld/LangGraphHelloWorld.py
@@ -1,3 +1,4 @@
+import uuid
 from typing import TypedDict
 
 from langgraph.graph import StateGraph, START, END
@@ -46,3 +47,61 @@ print(result['greeting'])
 """
 Hello, lz!.... 🙂
 """
+
+# 6. 打印图的边和节点信息
+# 打印图的ascii可视化结构   pip install grandalf
+print(app.get_graph().print_ascii())
+print("=" * 10)
+"""
++-----------+  
+| __start__ |  
++-----------+  
+      *        
+      *        
+      *        
+  +-------+    
+  | greet |    
+  +-------+    
+      *        
+      *        
+      *        
++-----------+  
+| add_emoji |  
++-----------+  
+      *        
+      *        
+      *        
+ +---------+   
+ | __end__ |   
+ +---------+   
+None
+"""
+
+# 打印mermaid代码
+print(app.get_graph().draw_mermaid())
+print("=" * 10)
+"""
+---
+config:
+  flowchart:
+    curve: linear
+---
+graph TD;
+	__start__([<p>__start__</p>]):::first
+	greet(greet)
+	add_emoji(add_emoji)
+	__end__([<p>__end__</p>]):::last
+	__start__ --> greet;
+	greet --> add_emoji;
+	add_emoji --> __end__;
+	classDef default fill:#f2f0ff,line-height:1.2
+	classDef first fill-opacity:0
+	classDef last fill:#bfb6fc
+"""
+
+# 生成.png并写入文件
+png_bytes = app.get_graph().draw_mermaid_png(max_retries=2, retry_delay=2.0)
+output_path = "langgraph" + str(uuid.uuid4())[:8] + ".png"
+with open(output_path, "wb") as f:
+    f.write(png_bytes)
+print(f"图片已生成: {output_path}")
